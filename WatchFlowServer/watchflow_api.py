cd0725af108488bb1de73bc88d69d98500014433
Caue Souza <caue.souzabarros@hotmail.com>
2021-05-10 22:37:22 -0300

    Changing gets to post and creating treatment for json and args

--- WatchFlowServer/watchflow_api.py
+++ WatchFlowServer/watchflow_api.py
@@ -1,190 +1,291 @@
 import flask
 from flask import request
-from Database import database
 import socket
+from Database import database
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/allRunningCameras', methods=['GET'])
-def getAllRunningCameras():
-    data = request.get_json()
+@app.route('/allRunningCameras', methods=['POST'])
+def allRunningCameras():
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
 
-    neededKeys = {'requesterUserId', 'requesterPwd'}
-
-    if neededKeys <= data.keys():
-        success, message = database.getCamerasDatabaseAsJSON(
-            requesterUserId=data['requesterUserId'],
-            requesterPwd=data['requesterPwd'])
-
-        return message, 200 if success else 400
-    else:
+    if dataJson is None and dataArgs is None:
         return 'Missing params', 400
 
-
-@app.route('/allCamerasIps', methods=['GET'])
-def getAllCamerasIps():
-    data = request.get_json()
-
-    neededKeys = {'requesterUserId', 'requesterPwd'}
-
-    if neededKeys <= data.keys():
-        success, message = database.getCamerasDatabaseAsJSON(
-            requesterUserId=data['requesterUserId'],
-            requesterPwd=data['requesterPwd'],
-            onlyIps=True)
-
-        return message, 200 if success else 400
     else:
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'requesterUserId', 'requesterPwd'}
+
+        if neededKeys <= data.keys():
+            success, message = database.getCamerasDatabaseAsJSON(
+                requesterUserId=data['requesterUserId'],
+                requesterPwd=data['requesterPwd'])
+
+            return message, 200 if success else 400
+        else:
+            return 'Missing params', 400
+
+
+@app.route('/allCamerasIps', methods=['POST'])
+def allCamerasIps():
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
+
+    if dataJson is None and dataArgs is None:
         return 'Missing params', 400
+
+    else:
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'requesterUserId', 'requesterPwd'}
+
+        if neededKeys <= data.keys():
+            success, message = database.getCamerasDatabaseAsJSON(
+                requesterUserId=data['requesterUserId'],
+                requesterPwd=data['requesterPwd'],
+                onlyIps=True)
+
+            return message, 200 if success else 400
+        else:
+            return 'Missing params', 400
 
 
 @app.route('/userLogin', methods=['POST'])
 def userLogin():
-    data = request.get_json()
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
 
-    neededKeys = {'userName', 'pwd', 'latitude', 'longitude'}
-
-    if neededKeys <= data.keys():
-        success, message = database.userLogin(
-            userName=data['userName'],
-            pwd=data['pwd'],
-            latitude=data['latitude'],
-            longitude=data['longitude'])
-
-        return message, 200 if success else 400
-    else:
+    if dataJson is None and dataArgs is None:
         return 'Missing params', 400
+
+    else:
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'userName', 'pwd', 'latitude', 'longitude'}
+
+        if neededKeys <= data.keys():
+            success, message = database.userLogin(
+                userName=data['userName'],
+                pwd=data['pwd'],
+                latitude=data['latitude'],
+                longitude=data['longitude'])
+
+            return message, 200 if success else 400
+        else:
+            return 'Missing params', 400
 
 
 @app.route('/userLogout', methods=['POST'])
 def userLogout():
-    data = request.get_json()
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
 
-    neededKeys = {'requesterUserId', 'requesterPwd'}
-
-    if neededKeys <= data.keys():
-        success, message = database.userLogout(
-            requesterUserId=data['requesterUserId'],
-            requesterPwd=data['requesterPwd'])
-
-        return message, 200 if success else 400
-    else:
+    if dataJson is None and dataArgs is None:
         return 'Missing params', 400
+
+    else:
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'requesterUserId', 'requesterPwd'}
+
+        if neededKeys <= data.keys():
+            success, message = database.userLogout(
+                requesterUserId=data['requesterUserId'],
+                requesterPwd=data['requesterPwd'])
+
+            return message, 200 if success else 400
+        else:
+            return 'Missing params', 400
 
 
 @app.route('/registerUser', methods=['POST'])
 def registerUser():
-    data = request.get_json()
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
 
-    neededKeys = {'requesterUserId',
-                  'requesterPwd', 'newUserName', 'newUserPwd'}
-
-    if neededKeys <= data.keys():
-        success, message = database.createUser(
-            requesterUserId=data['requesterUserId'],
-            requesterPwd=data['requesterPwd'],
-            newUserName=data['newUserName'],
-            newUserPwd=data['newUserPwd'],
-            userType=data['type'] if ('type' in data) else 0)
-
-        return message, 200 if success else 400
+    if dataJson is None and dataArgs is None:
+        return 'Missing params', 400
 
     else:
-        return 'Missing params', 400
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'requesterUserId',
+                      'requesterPwd', 'newUserName', 'newUserPwd'}
+
+        if neededKeys <= data.keys():
+            success, message = database.createUser(
+                requesterUserId=data['requesterUserId'],
+                requesterPwd=data['requesterPwd'],
+                newUserName=data['newUserName'],
+                newUserPwd=data['newUserPwd'],
+                userType=data['type'] if ('type' in data) else 0)
+
+            return message, 200 if success else 400
+
+        else:
+            return 'Missing params', 400
 
 
 @app.route('/deleteUser', methods=['DELETE'])
 def deleteUser():
-    data = request.get_json()
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
 
-    neededKeys = {'requesterUserId', 'requesterPwd', 'oldUserId'}
-
-    if neededKeys <= data.keys():
-        success, message = database.deleteUser(
-            requesterUserId=data['requesterUserId'],
-            requesterPwd=data['requesterPwd'],
-            oldUserName=data['oldUserName'])
-
-        return message, 200 if success else 400
+    if dataJson is None and dataArgs is None:
+        return 'Missing params', 400
 
     else:
-        return 'Missing params', 400
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'requesterUserId', 'requesterPwd', 'oldUserId'}
+
+        if neededKeys <= data.keys():
+            success, message = database.deleteUser(
+                requesterUserId=data['requesterUserId'],
+                requesterPwd=data['requesterPwd'],
+                oldUserName=data['oldUserName'])
+
+            return message, 200 if success else 400
+
+        else:
+            return 'Missing params', 400
 
 
 @app.route('/registerCamera', methods=['POST'])
 def registerCamera():
-    data = request.get_json()
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
 
-    neededKeys = {'requesterUserId', 'requesterPwd',
-                  'cameraIp', 'latitude', 'longitude'}
-
-    if neededKeys <= data.keys():
-        success, message = database.createCamera(
-            requesterUserId=data['requesterUserId'],
-            requesterPwd=data['requesterPwd'],
-            cameraIp=data['cameraIp'],
-            latitude=data['latitude'],
-            longitude=data['longitude'])
-
-        return message, 200 if success else 400
-    else:
+    if dataJson is None and dataArgs is None:
         return 'Missing params', 400
+
+    else:
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'requesterUserId', 'requesterPwd',
+                      'cameraIp', 'latitude', 'longitude'}
+
+        if neededKeys <= data.keys():
+            success, message = database.createCamera(
+                requesterUserId=data['requesterUserId'],
+                requesterPwd=data['requesterPwd'],
+                cameraIp=data['cameraIp'],
+                latitude=data['latitude'],
+                longitude=data['longitude'])
+
+            return message, 200 if success else 400
+        else:
+            return 'Missing params', 400
 
 
 @app.route('/deleteCamera', methods=['DELETE'])
 def deleteCamera():
-    data = request.get_json()
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
 
-    neededKeys = {'requesterUserId', 'requesterPwd', 'cameraIp'}
-
-    if neededKeys <= data.keys():
-        success, message = database.deleteCamera(
-            requesterUserId=data['requesterUserId'],
-            requesterPwd=data['requesterPwd'],
-            cameraIp=data['cameraIp'])
-
-        return message, 200 if success else 400
+    if dataJson is None and dataArgs is None:
+        return 'Missing params', 400
 
     else:
-        return 'Missing params', 400
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'requesterUserId', 'requesterPwd', 'cameraIp'}
+
+        if neededKeys <= data.keys():
+            success, message = database.deleteCamera(
+                requesterUserId=data['requesterUserId'],
+                requesterPwd=data['requesterPwd'],
+                cameraIp=data['cameraIp'])
+
+            return message, 200 if success else 400
+
+        else:
+            return 'Missing params', 400
 
 
 @app.route('/updateCamera', methods=['POST'])
 def updateCamera():
-    data = request.get_json()
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
 
-    neededKeys = {'requesterUserId', 'requesterPwd', 'cameraIp', 'snapshot'}
-
-    if neededKeys <= data.keys():
-        success, message = database.updateCamera(
-            requesterUserId=data['requesterUserId'],
-            requesterPwd=data['requesterPwd'],
-            cameraIp=data['cameraIp'],
-            snapshot=data['snapshot'])
-
-        return message, 200 if success else 400
+    if dataJson is None and dataArgs is None:
+        return 'Missing params', 400
 
     else:
-        return 'Missing params', 400
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'requesterUserId',
+                      'requesterPwd', 'cameraIp', 'snapshot'}
+
+        if neededKeys <= data.keys():
+            success, message = database.updateCamera(
+                requesterUserId=data['requesterUserId'],
+                requesterPwd=data['requesterPwd'],
+                cameraIp=data['cameraIp'],
+                snapshot=data['snapshot'])
+
+            return message, 200 if success else 400
+
+        else:
+            return 'Missing params', 400
 
 
 @app.route('/usersPositions', methods=['POST'])
 def usersPositions():
-    data = request.get_json()
+    dataJson = request.get_json()
+    dataArgs = request.args.to_dict()
 
-    neededKeys = {'requesterUserId', 'requesterPwd'}
-
-    if neededKeys <= data.keys():
-        success, jsonOutput = database.getAllLoggedUsersPositions(
-            requesterUserId=data['requesterUserId'],
-            requesterPwd=data['requesterPwd'])
-
-        return jsonOutput, 200 if success else 400
+    if dataJson is None and dataArgs is None:
+        return 'Missing params', 400
 
     else:
-        return 'Missing params', 400
+        if dataArgs is None:
+            data = dataJson
+        else:
+            data = dataArgs
+
+        neededKeys = {'requesterUserId', 'requesterPwd'}
+
+        if neededKeys <= data.keys():
+            success, jsonOutput = database.getAllLoggedUsersPositions(
+                requesterUserId=data['requesterUserId'],
+                requesterPwd=data['requesterPwd'])
+
+            return jsonOutput, 200 if success else 400
+
+        else:
+            return 'Missing params', 400
 
 
 if __name__ == '__main__':
