f3ef17b7a48286c95f20c81eb917b17da6627bec
Caue Souza <caue.souzabarros@hotmail.com>
2021-05-15 10:07:07 -0300

    menu refactor

--- WatchFlowApp/app/src/main/java/com/example/watchflow/MapsActivity.java
+++ WatchFlowApp/app/src/main/java/com/example/watchflow/MapsActivity.java
@@ -1,14 +1,19 @@
 package com.example.watchflow;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.watchflow.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends AppCompatActivity {
 
     MapsViewModel viewModel;
     ActivityMapsBinding binding;
@@ -23,22 +28,54 @@ public class MapsActivity extends FragmentActivity {
 
         MapFragment mapFragment = new MapFragment();
 
+        getSupportActionBar().setTitle(R.string.app_name);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().show();
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.maps_layout, mapFragment)
                 .commit();
-
-        FabsFragment fabsFragment = new FabsFragment();
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.maps_layout, fabsFragment)
-                .commit();
-
-        initBindings();
     }
 
-    public void initBindings() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
 
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.refresh_item:
+                viewModel.getRefreshEvent().call();
+                return true;
+            case R.id.add_user_item:
+                viewModel.getAddUserEvent().call();
+                return true;
+            case R.id.remove_user_item:
+                viewModel.getRemoveUserEvent().call();
+                return true;
+            case R.id.add_cam_item:
+                viewModel.getAddCamEvent().call();
+                return true;
+            case R.id.remove_cam_item:
+                viewModel.getRemoveCamEvent().call();
+                return true;
+            case R.id.logout_item:
+                viewModel.getLogoutEvent().call();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
